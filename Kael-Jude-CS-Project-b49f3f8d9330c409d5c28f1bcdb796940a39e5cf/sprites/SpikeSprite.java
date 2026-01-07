2a930ed95ea1c924248de6c5681621ce42bad11e
38069008 <38069008@L829WD220020.edu.cbe.ab.ca>
2026-01-07 10:57:03 -0700

    level 2 in progress

--- Kael-Jude-CS-Project-b49f3f8d9330c409d5c28f1bcdb796940a39e5cf/sprites/SpikeSprite.java
+++ Kael-Jude-CS-Project-b49f3f8d9330c409d5c28f1bcdb796940a39e5cf/sprites/SpikeSprite.java
@@ -5,142 +5,143 @@ import javax.imageio.ImageIO;
 
 public class SpikeSprite implements DisplayableSprite {
 
-    private static final String DEFAULT_IMAGE_PATH = "res/red.jpg";
-    private Image image;
+	private static final String DEFAULT_IMAGE_PATH = "res/red.jpg";
+	private Image image;
 
-    private double centerX;
-    private double centerY;
-    private double width;
-    private double height;
-    private boolean dispose;
-    private double obSpeed;
-    private String imagePath;
-    private double velocityX;
+	private double centerX;
+	private double centerY;
+	private double width;
+	private double height;
+	private boolean dispose;
+	private double obSpeed;
+	private String imagePath;
+	private double velocityX;
 
-    public SpikeSprite(double centerX, double centerY, double width, double height) {
-        this(centerX, centerY, width, height, DEFAULT_IMAGE_PATH);
-    }
-
-    public SpikeSprite(double centerX, double centerY, double width, double height, String imagePath) {
-        this.centerX = centerX;
-        this.centerY = centerY;
-        this.width = width;
-        this.height = height;
-        this.imagePath = imagePath;
-        loadImage(imagePath);
-    }
-    
-    
-    public String getImagePath() {
-    	return imagePath;
-    }
-    
-    private void loadImage(String imagePath) {
-        try {
-            image = ImageIO.read(new File(imagePath));
-            if (image == null) {
-                System.err.println("Image not found: " + imagePath);
-            }
-        } catch (IOException e) {
-            System.err.println("Error loading image: " + e);
-            image = null;
-        }
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public boolean getVisible() {
-        return true;
-    }
-
-    public double getMinX() { 
-    	return centerX - (width / 2); 
-	}
-    
-    public double getMaxX() { 
-    	return centerX + (width / 2); 
-	}
-    
-    public double getMinY() { 
-    	return centerY - (height / 2);
-	}
-    
-    public double getMaxY() {
-    	return centerY + (height / 2);
-	}
-    
-    public double getHeight() { 
-    	return height; 
-	}
-    
-    public double getWidth() { 
-    	return width;
-	}
-    
-    public double getCenterX() {
-    	return centerX;
-	}
-    
-    public double getCenterY() { 
-    	return centerY; 
-	}
-    
-    public boolean getDispose() { 
-    	return dispose; 
+	public SpikeSprite(double centerX, double centerY, double width, double height) {
+		this(centerX, centerY, width, height, DEFAULT_IMAGE_PATH);
 	}
 
-    public void setCenterX(double centerX) { 
-    	this.centerX = centerX; 
+	public SpikeSprite(double centerX, double centerY, double width, double height, String imagePath) {
+		this.centerX = centerX;
+		this.centerY = centerY;
+		this.width = width;
+		this.height = height;
+		this.imagePath = imagePath;
+		loadImage(imagePath);
 	}
-    
-    public void setCenterY(double centerY) { 
-    	this.centerY = centerY; 
+
+
+	public String getImagePath() {
+		return imagePath;
 	}
-    
-    @Override
-    public void setDispose(boolean dispose) { 
-    	this.dispose = dispose; 
+
+	private void loadImage(String imagePath) {
+		try {
+			image = ImageIO.read(new File(imagePath));
+			if (image == null) {
+				System.err.println("Image not found: " + imagePath);
+			}
+		} catch (IOException e) {
+			System.err.println("Error loading image: " + e);
+			image = null;
+		}
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public boolean getVisible() {
+		return true;
+	}
+
+	public double getMinX() { 
+		return centerX - (width / 2); 
+	}
+
+	public double getMaxX() { 
+		return centerX + (width / 2); 
+	}
+
+	public double getMinY() { 
+		return centerY - (height / 2);
+	}
+
+	public double getMaxY() {
+		return centerY + (height / 2);
+	}
+
+	public double getHeight() { 
+		return height; 
+	}
+
+	public double getWidth() { 
+		return width;
+	}
+
+	public double getCenterX() {
+		return centerX;
+	}
+
+	public double getCenterY() { 
+		return centerY; 
+	}
+
+	public boolean getDispose() { 
+		return dispose; 
+	}
+
+	public void setCenterX(double centerX) { 
+		this.centerX = centerX; 
+	}
+
+	public void setCenterY(double centerY) { 
+		this.centerY = centerY; 
+	}
+
+	@Override
+	public void setDispose(boolean dispose) { 
+		this.dispose = dispose; 
 	}
 
 	public void update(Universe universe, long actualDeltaTime) {
 		double deltaTime = actualDeltaTime * 0.001;
+
+		ShellUniverse u = (ShellUniverse) universe;
+		obSpeed = u.getObSpeed();
+
+
+		KeyboardInput keyboard = KeyboardInput.getKeyboard();
+
+
+
+		if (keyboard.keyDown(39)) { // ob moves right objects move left
+			velocityX -= obSpeed * deltaTime;
+			centerX += velocityX * deltaTime;
+		}
+
+		else if (keyboard.keyDown(37)) {
+			velocityX += obSpeed * deltaTime;
+			centerX += velocityX * deltaTime;
+		}
+
+		else  if (velocityX > 0) { // and vice versa 
+			velocityX -= obSpeed * deltaTime;
+			centerX += velocityX * deltaTime;
+		}
+		else if (velocityX < 0) {
+			velocityX += obSpeed * deltaTime;
+			centerX += velocityX * deltaTime;
+		}
+
 		
-	    ShellUniverse u = (ShellUniverse) universe;
-	    obSpeed = u.getObSpeed();
-	    
+		// small bugfix to fix micro movements
+		if (velocityX < 0 && velocityX > -1) {
+			velocityX = 0;
+		}
+		if (velocityX > 0 && velocityX < 1) {
+			velocityX = 0;
+		}
 		
-	    KeyboardInput keyboard = KeyboardInput.getKeyboard();
-
-
-
-        if (keyboard.keyDown(39)) { // ob moves right objects move left
-        	velocityX -= obSpeed * deltaTime;
-            centerX += velocityX * deltaTime;
-        }
-        
-        else if (keyboard.keyDown(37)) {
-        	velocityX += obSpeed * deltaTime;
-            centerX += velocityX * deltaTime;
-        }
-        
-        else  if (velocityX > 0) { // and vice versa 
-        	velocityX -= obSpeed * deltaTime;
-            centerX += velocityX * deltaTime;
-        }
-        else if (velocityX < 0) {
-        	velocityX += obSpeed * deltaTime;
-        	centerX += velocityX * deltaTime;
-        }
-        
-        
-        // small bugfix to fix micro movements
-        if (velocityX < 0 && velocityX > -1) {
-        	velocityX = 0;
-        }
-        if (velocityX > 0 && velocityX < 1) {
-        	velocityX = 0;
-        }
 	}
 }
--- Kael-Jude-CS-Project-b49f3f8d9330c409d5c28f1bcdb796940a39e5cf/universes/ShellUniverse.java
+++ Kael-Jude-CS-Project-b49f3f8d9330c409d5c28f1bcdb796940a39e5cf/universes/ShellUniverse.java
@@ -14,7 +14,7 @@ public class ShellUniverse implements Universe {
     private double wallSpeed;
     private String currentLevelPath;
     private boolean resetLevel = false;
-    private String[] levels = {"res/LevelData/level1.txt", "res/LevelData/level2.txt"};
+    private String[] levels = {"res/LevelData/level2.txt", "res/LevelData/level1.txt", "res/LevelData/level2.txt"};
     private int currentLevelIndex = 0;
     private boolean nextLevel = false;
 
